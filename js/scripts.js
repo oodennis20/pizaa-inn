371cfa5f2b63eba2d3c53ad65666ae0c1653be01
oodennis20 <oodennis20@gmail.com>
2019-10-07 03:21:08 +0300

    add function to submit button

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,43 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "20px";
   }
 }
+
+$("form#order").submit(function(event) {
+  event.preventDefault();
+
+  // the data from the form
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+
+  var crust = $("select#IG2").val();
+  var size = $("select#IG1").val();
+  var toppings = [];
+  $.each($("input[name='toppings']:checked"), function(){
+      toppings.push($(this).val());
+  });
+  var area = $("input#area-name").val();
+  var apartment=$("input#apartment-no").val();
+  var door = $("input#door-no").val();
+  var estate = $("input#estate").val();
+
+  function order(name,email,size,crust,toppings){
+    this.Name = name;
+    this.Email = email;
+    this.Size = size;
+    this.Crust = crust;
+    this.Toppings = toppings;
+  }
+
+  var neworder = new order(name,email,size,toppings,crust);
+  console.log(neworder)
+
+  resetFields();
+
+});
+function resetFields(){
+  $("input#name").val("");
+  $("input#email").val("");
+  $("input[name='toppings']").prop('checked',false);
+  $("select#IG2").val("");
+  $("select#IG1").val("");
+};
