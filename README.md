3bbb4b2ac7eb079dee016f1c28ee2d9bac0b66be
oodennis20 <oodennis20@gmail.com>
2019-10-14 08:20:26 +0300

    update README file

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 #### The Pizaa inn, 5th October 2019
 #### By **Dennis Otieno**
 ## Description
-This application allows the users to view the services offered by Pizaa inn which enables them to choose the size of pizaa of their choice and be able to order for them online. The users also have the option of whether to be delivered for or not. I f the user wants the delivery to be successful then the email,areaname,apartment number and the specific door number should be provided.
+This app allows our users and visitors to order pizza as they decide which toppings they preffer and crust type for their pizzas. they get to decide whether they want small or large. Users get to see their total bill at the end of every order made. The app also prompts users whether they would like their pizza delivered at their door steps.
 ## Technologies Used
 the application uses html and java script 
 ## Support and contact details
 oodennis@gmail.com
 ## BDD
-Once the user clicks on the submit button it will automatically reset the fields the user had already filled. The user also has the choice of making as many orders as they want as long as after filling the required fields the user clicks on the 'anotherorder' button which will save the users first choices of the crust,toppings and also the size. After the user is done , should click on the submit button which will calculate the total/grand cost that the user is to be charged. 
+The app allows the user to choose several toppings, one crust and sizes they prefer. Using this data or input I get the price of the toppings, crust and size of each and every pizza. We have delivery services which have a constant fee of 200 shillings. I use this data I get to calculate the total amount the user will be needed to pay. the price may range for one who requires a delivery.
 ### License
 MIT License Copyright (c) 2019 **Dennis Otieno**
--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,184 @@
-window.onscroll = function() {scrollFunction()};
+ window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "20px";
-  }
-}
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     document.getElementById("navbar").style.top = "0";
+   } else {
+     document.getElementById("navbar").style.top = "20px";
+   }
+ }
 
-class order {
-  constructor(name, email, size, crust, toppings) {
-    this.Name = name;
-    this.Email = email;
-    this.Size = size;
-    this.Crust = crust;
-    this.Toppings = toppings;
-  }
-}
+// constructor for the order
+function Order(name,email,size,crust,toppings){
+  this.Name = name;
+  this.Email = email;
+  this.Size = size;
+  this.Crust = crust;
+  this.Toppings = [toppings];
+  this.Addresses = [];
 
-$("form#order").submit(function(event) {
-  event.preventDefault();
+};
 
+// constructor for the address
+function Address(area,estate,apartment,door){
+  this.Area = area;
+  this.Estate = estate;
+  this.Apartment = apartment;
+  this.Door = door;
+};
 
-  var name = $("input#name").val();
-  var email = $("input#email").val();
-
-  var crust = $("select#IG2").val();
-  var size = $("select#IG1").val();
-  var toppings = [];
-  $.each($("input[name='toppings']:checked"), function(){
-      toppings.push($(this).val());
-  });
-  var area = $("input#area-name").val();
-  var apartment=$("input#apartment-no").val();
-  var door = $("input#door-no").val();
-  var estate = $("input#estate").val();
-
- 
-
-  var neworder = new order(name,email,size,toppings,crust);
-  console.log(neworder)
-
-  resetFields()
-  function resetFields(){
-    $("input#name").val("");
-    $("input#email").val("");
-    $("input[name='toppings']").prop('checked',false);
-    $("select#IG2").val("");
-    $("select#IG1").val("");
-  };
-
+$("#delivery").click(function(){
+  alert("Our Delivery fee is KSh.150");
+  $(".addr").show();
+  $(".addrss").show();
 });
 
 
-calculation(neworder);
+// form submission event
+$("form#new-order").submit(function(event) {
+  event.preventDefault();
 
-function calculation(neworder){
-  var sizeprice=parseInt(neworder.Size.split(", ")[1]);
-  console.log(sizeprice);
+
+  // the data from the form
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+  var crust = $("select#IG2").val();
+  var size = $("select#IG1").val();
+  var top = [];
+  $.each($("input[name='topping']:checked"), function(){
+      top.push($(this).val());
+  });
+
+  var area = $("input#areaname").val();
+  var estate = $("input#estate").val();
+  var apartment = $("input#apartment-no").val();
+  var door = $("input#door-no").val();
+
+
+  // Structure of th constructor ***Order(name,phone,size,crust,toppings )***
+  // creating the object for the order made by the user
+  var newOrder = new Order(name,email,size,crust,top);
+  console.log(newOrder);
+
+
+  // structure for the constructor ***Address(street,estate,apartment,floor)***
+  // creating the object for the address given for delivery
+  var newAddress = new Address(area,estate,apartment,door);
+  console.log(newAddress);
+  newOrder.Addresses.push(newAddress);
+
+
+  billCalculation(newOrder);
+
+  displayData(newOrder);
+
+  // putting the orders into an array
+  var totalOrders = [];
+  totalOrders.push(newOrder);
+  console.log(totalOrders);
+
+
+  // this is for emptying the fields after clicking submit
+ 
+  resetFields();
+
+});
+
+// CALCULATION FUNCTION
+function billCalculation(newOrder){
+  var sizePrice = parseInt(newOrder.Size.split(", ")[1]);
+  console.log(sizePrice);
+
+  var crustPrice = parseInt(newOrder.Crust.split(", ")[1]);
+  console.log(crustPrice);
+
+  
+  var topp = newOrder.Toppings;
+  console.log(topp);
+
+  var toppingPrice = 0;
+
+  // for getting toppings
+  for(var i = 0; i < topp.length; i++ ){
+      var unitPrice = parseInt(topp[i].split(", ")[1]);
+
+      toppingPrice += unitPrice;
+  }
+  
+  function deliveryFee(){
+      var delivery = newOrder.Addresses;
+      console.log(delivery);
+      var deliveryPrice;
+
+      if(delivery[0].Estate==null || delivery[0].Estate=="" && delivery[0].Area==null || delivery[0].Area=="" && delivery[0].Apartment==null || delivery[0].Apartment=="" && delivery[0].Door==null || delivery[0].Door==""){
+          deliveryPrice = 0;
+          return deliveryPrice;
+      }else{
+          deliveryPrice = 150;
+          return deliveryPrice;
+      };
+  }
+
+  var deliveryTag = deliveryFee();
+  console.log(deliveryTag);
+
+  var price = sizePrice + crustPrice + toppingPrice + deliveryTag;
+  console.log(price);
+
+  // total
+  $("#total").text(price);
+
 }
+
+
+
+// Structure of th constructor ***Order(name,phone,size,crust,toppings )***
+// function for displaying  data
+function displayData(newOrder){
+  var name = newOrder.Name;
+  var email = newOrder.Email;
+
+  var size = newOrder.Size.split(", ")[0];
+  var sizePrice = newOrder.Size.split(", ")[1];
+
+  var crust = newOrder.Crust.split(", ")[0];
+  var crustPrice = newOrder.Crust.split(", ")[1];
+
+  var toppings;
+  var toppingsPrice;
+
+  var tops = newOrder.Toppings;
+  tops.forEach(function(topps) {
+     
+      toppings = topps.split(", ")[0];
+      toppingsPrice = topps.split(", ")[1]; 
+      // displaying the list of arrays
+      $("ul#new-topping").append("<li><span class=''>" + toppings + " " + " -KSh." + toppingsPrice + "</span></li>");
+  });
+
+  // print outs
+  $("#new-name").text(name);
+  $("#new-email").text(email);
+  $("#new-size").text(size + " -KSh." + sizePrice);
+  $("#new-crust").text(crust + " -KSh." + crustPrice);
+
+  // printing the address
+  var ad = newOrder.Addresses;
+  $("#new-estate").text(ad[0].Estate);
+  $("#new-area").text(ad[0].Area);
+  $("#new-apartment").text(ad[0].Apartment);
+  $("#new-door").text(ad[0].Door);
+
+}
+
+
+// for emptying the fields
+
+
+function resetFields(){
+  $("input#name").val("");
+  $("input#email").val("");
+  $("input[name='topping']").prop('checked',false);
+  $("select#IG2").val("");
+  $("select#IG1").val("");
+};
